12fd8cffc7c26baa445d6a34ae7d49bbe91a77ea
Cinthia <cinthiaym_@outlook.com>
2020-10-31 23:40:27 -0300

    Add Constraint layout na tela de registro

--- app/src/main/java/com/google/codelabs/mdc/java/firebaseAuth/SignUpActivity.java
+++ app/src/main/java/com/google/codelabs/mdc/java/firebaseAuth/SignUpActivity.java
@@ -2,11 +2,13 @@ package com.google.codelabs.mdc.java.firebaseAuth;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
 import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
